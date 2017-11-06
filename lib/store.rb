09f558f35ff7e5abe970f7681d5ff818e077282b
Lex Yehia <lex.yehia@icloud.com>
2017-11-06 15:36:23 -0500

    Done Exercise 9

--- lib/store.rb
+++ lib/store.rb
@@ -4,9 +4,18 @@ class Store < ActiveRecord::Base
   validates :annual_revenue, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validate :must_carry_some_apparels
 
+  before_destroy :no_destruct_if_has_employees
+
+  private
   def must_carry_some_apparels
     if (@mens_apparel == false && @womens_apparel == false)
       errors.add(:store_id, "must have either men's or women's apprels as true")
+    end
+  end
+
+  def no_destruct_if_has_employees
+    if (self.employees.size > 0)
+      throw :abort
     end
   end
 end
